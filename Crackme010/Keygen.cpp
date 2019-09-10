26c99c60038e30c988538b28c96fc2e27d944022
UnreachableLove <44860401+UnreachableLove@users.noreply.github.com>
2019-09-10 19:23:26 +0800

    Add files via upload

---
+++ Crackme010/Keygen.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+
+int Key()
+{
+	char szKey[30] = "kXy^rO|*yXo*m\\kMuOn*+";
+	int NameLen = 0;
+
+	NameLen = strlen(szKey);
+
+	for (int i = 0; i < NameLen; i++)
+	{
+		szKey[i] -= 0xa;
+	}
+	printf("%s", szKey);
+	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	Key();
+	return 0;
+}

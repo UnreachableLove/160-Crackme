66f48acb9cc4d339bc447855c06d86ad92aeaa2e
UnreachableLove <44860401+UnreachableLove@users.noreply.github.com>
2019-09-20 19:43:52 +0800

    Add files via upload

---
+++ Crackme019/Keygen.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <Windows.h>
+
+int Key()
+{
+	char szName[20] = { 0 };
+	int NameLen = 0;
+	int code = 0x81276345;
+
+	printf("请输入用户名:");
+	scanf_s("%s", szName, 20);
+
+	NameLen = strlen(szName);
+	for (int i = 0; i < NameLen; i++)
+	{
+		code += szName[i];
+		code = code ^ (i << 8);
+		code = code * ((~(NameLen * i)) * (i + 1));
+	}
+
+	printf("%lu", code);
+	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	Key();
+	return 0;
+}
